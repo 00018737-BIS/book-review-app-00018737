4b34fa9258593464424ea05911349c4f83e3ec96
00018737-BIS <00018737student@gmail.com>
2025-04-05 16:10:41 +0500

    Added public routes files. Made the routing also fixed app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const { body, validationResult } = require('express-validator'); // Add this
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const indexRouter = require('./routes/index');
+const reviewsRouter = require('./routes/reviews');
+
+app.use('/', indexRouter);
+app.use('/reviews', reviewsRouter);
+
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+    console.log(App listening on port ${PORT});
 });
